387bace0719db16e66497c6bd974d73a3008266e
Dell <amerakhames42@gmail.com>
2025-06-24 01:15:14 +0300

    final project

--- todo-app-project/src/Redux/todosSlice.js
+++ todo-app-project/src/Redux/todosSlice.js
@@ -8,32 +8,35 @@ export const FILTRATION_TYPES = {
 };
 
 const todosSlice = createSlice({
-  name:"todos",
-  initialState:{todos:[],
+  name: "todos",
+  initialState: {
+    todos: [],
     filter: FILTRATION_TYPES.ALL,
   },
-  reducers:{
-addToDo:(state , action)=>{
-  const newTodo = action.payload;
-  state.todos.push(newTodo);
-},
-removeToDo:(state , action)=>{
-  const id = action.payload;
-  state.todos = state.todos.filter((todo)=> todo.id != id)
-},
-completedToDo:(state , action)=>{
-  const id = action.payload;
-  const todo = state.todos.find((todo)=> todo.id == id)
-  if(todo){
-    todo.completed = !todo.completed 
-  }
-
-},
-updateFilter:(state , action)=>{
-  const filter = action.payload;
-  state.filter = filter
-}
+  reducers: {
+    addToDo: (state, action) => {
+      const newTodo = action.payload;
+      state.todos.push(newTodo);
+    },
+    removeToDo: (state, action) => {
+      const id = action.payload;
+      state.todos = state.todos.filter((todo) => todo.id != id)
+    },
+    completedToDo: (state, action) => {
+      const id = action.payload;
+      const todo = state.todos.find((todo) => todo.id == id)
+      if (todo) {
+        todo.completed = !todo.completed
+      }
+    },
+    setTodos: (state, action) => {
+      state.todos = action.payload;
+    },
+    updateFilter: (state, action) => {
+      const filter = action.payload;
+      state.filter = filter
+    }
   }
 })
-export const {addToDo , removeToDo , completedToDo , updateFilter} = todosSlice.actions
+export const { addToDo, removeToDo, completedToDo, updateFilter , setTodos } = todosSlice.actions
 export default todosSlice;
--- todo-app-project/src/components/TodoItems.js
+++ todo-app-project/src/components/TodoItems.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FlatList  , Text} from 'react-native'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { styles }  from '../../styles'
-import { FILTRATION_TYPES, updateFilter } from '../Redux/todosSlice';
+import { FILTRATION_TYPES, setTodos, updateFilter } from '../Redux/todosSlice';
 import TodoList from './TodoList';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const TodoItems = () => {
+    const dispatch = useDispatch();
     const { todos , filter } = useSelector((state) => state.todo)
-
+    useEffect(() => {
+        const loadTodos = async () => {
+            try {
+                const saved = await AsyncStorage.getItem('TODOS');
+                if (saved !== null) {
+                    dispatch(setTodos(JSON.parse(saved)));
+                    console.log(saved);
+                }
+            } catch (e) {
+                console.error('Failed to load todos:', e);
+            }
+        };
+        loadTodos();
+    }, []);
+    useEffect(() => {
+        saveTodos();
+    }, [todos]);
+    const saveTodos = async () => {
+        try {
+            await AsyncStorage.setItem('TODOS', JSON.stringify(todos));
+        } catch (e) {
+            console.error('Failed to save todos:', e);
+        }
+    };
   return (
    <>
           <FlatList
